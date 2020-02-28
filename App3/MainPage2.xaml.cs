296d532d428ec7d8c6146715100c8affe65d303a
134130 <a1eng0@daum.net>
2020-02-29 03:07:04 +0900

    first commit

---
+++ App3/MainPage2.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234238에 나와 있습니다.
+
+namespace App3
+{
+    /// <summary>
+    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class MainPage2 : Page
+    {
+        int year = DateTime.Now.Year;
+        int shtm = DateTime.Now.Month < 5 ? 1 : 2;
+        int line = 2;
+
+        List<List<Border>> borderList = new List<List<Border>>();
+        List<List<string>> textList = new List<List<string>>();
+
+        Payload payload;
+        public MainPage2()
+        {
+            this.InitializeComponent();
+            textBlock_Year.Text = year.ToString() + "년 " + shtm.ToString() + "학기";
+            CreateLine(1);
+            borderList[0][0].Visibility = Visibility.Visible;
+            borderList[0][1].Visibility = Visibility.Visible;
+
+            for (int i=2; i<=10; i++)
+            {
+                CreateLine(i);
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (line < 11)
+            {
+                borderList[line - 1][0].Visibility = Visibility.Visible;
+                borderList[line - 1][1].Visibility = Visibility.Visible;
+                line++;
+            }
+            
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox tb;
+            for (int i=1; i<line; i++)
+            {
+                textList.Add(new List<string>());
+                tb = (TextBox)borderList[line - 1][0].Child;
+                textList[i - 1].Add(tb.Text);
+                tb = (TextBox)borderList[line - 1][1].Child;
+                textList[i - 1].Add(tb.Text);
+            }
+
+            Payload payload2 = new Payload();
+            payload2.ans1 = payload.ans1;
+            payload2.ans2 = payload.ans2;
+            payload2.ans3 = year.ToString();
+            payload2.ans4 = shtm.ToString();
+            payload2.ans5 = textList;
+            this.Frame.Navigate(typeof(MainPage3), payload2);
+        }
+
+        public Border CreateBorder(int row, int col)
+        {
+            Border bd = new Border();
+            bd.VerticalAlignment = VerticalAlignment.Center;
+            Grid.SetRow(bd, row);
+            Grid.SetColumn(bd, col);
+            TextBox tbox = new TextBox();
+            tbox.Margin = new Thickness(col==0 ? 40:20, 0, 0, 0);
+            tbox.PlaceholderText = col == 0 ? "010001" : "01";
+            bd.Child = tbox;
+            bd.Visibility = Visibility.Collapsed;
+
+            return bd;
+        }
+
+        private void CreateLine(int index)
+        {
+            RowDefinition rd = new RowDefinition();
+            rd.Height = new GridLength(40.0);
+            sugangList.RowDefinitions.Add(rd);
+
+            borderList.Add(new List<Border>());
+            borderList[index-1].Add(CreateBorder(index, 0));
+            borderList[index-1].Add(CreateBorder(index, 1));
+            sugangList.Children.Add(borderList[index-1][0]);
+            sugangList.Children.Add(borderList[index-1][1]);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            payload = (Payload)e.Parameter;
+        }
+    }
+}
---
+++ App3/MainPage3.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Syndication;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234238에 나와 있습니다.
+
+namespace App3
+{
+    public sealed partial class MainPage3 : Page
+    {
+        static Payload payload;
+        static HttpClient httpClient;
+        static readonly String host = "https://shiis.uhs.ac.kr/";
+
+        public MainPage3()
+        {
+            this.InitializeComponent();
+            currentTime.Text = DateTime.Now.ToString();
+
+            TimeSpan period = TimeSpan.FromSeconds(0.2);
+            ThreadPoolTimer PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (source) =>
+            {
+                // TODO: Work
+                DateTime now = DateTime.Now;
+                //////////////////
+                await Dispatcher.RunAsync(CoreDispatcherPriority.High,
+                    () =>
+                    {
+                        currentTime.Text = now.ToString();
+                    });
+            }, period);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            payload = (Payload)e.Parameter;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IAsyncAction asyncAction = ThreadPool.RunAsync((workItem) =>
+            {
+                _ = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                CoreDispatcherPriority.High, new DispatchedHandler(() =>
+                {
+                    statusText.Text = "Hi";
+                }));
+            });
+        }
+
+        private async void Login_Button(object sender, RoutedEventArgs e)
+        {
+            httpClient = new HttpClient();
+            var url = host + "hsudoc/sys.Login.doj";
+            var parameters = new Dictionary<string, string> {
+                { "strCommand", "LOGIN"}, { "strTarget", "MAIN" },
+                { "strLoginId", payload.ans1}, { "strLoginPw", payload.ans2} };
+            var encodedContent = new FormUrlEncodedContent(parameters);
+            var headers = httpClient.DefaultRequestHeaders;
+            try
+            {
+                headers.UserAgent.TryParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36");
+            } catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            //Uri reqUri = new Uri(host + "hsudoc/sys.Login.doj");
+            HttpResponseMessage httpRes;
+
+
+            httpRes = await httpClient.PostAsync(url, encodedContent);
+            //var httpResBody = httpRes.Content.ReadAsStringAsync();
+            //Debug.WriteLine(httpResBody.Result);
+            statusText.Text = "Login Success";
+            statusText.Text = httpRes.Headers.ToString();
+
+        }
+
+        private async void Regist_Button(object sender, RoutedEventArgs e)
+        {
+            var url = host + "hsudoc/sug.SugSugangMng.do";
+            List<FormUrlEncodedContent> encodedContents = new List<FormUrlEncodedContent>();
+            foreach (List<string> ans in payload.ans5)
+            {
+                var parameter = new Dictionary<string, string> {
+                { "strCommand", "Save1" }, { "strSugangYy", payload.ans3 },
+                { "strSugangShtm", payload.ans4 }, { "strDanFg", "1" },
+                { "strSbjtCd",  ans[0]}, { "strDecs", String.Format("{00}", ans[1]) } };
+                encodedContents.Add(new FormUrlEncodedContent(parameter));
+            }
+
+            HttpResponseMessage httpRes = new HttpResponseMessage();
+            foreach(var encodedContent in encodedContents)
+            {
+                statusText.Text = "Suggest";
+                try { 
+                    httpRes = await httpClient.PostAsync(url, encodedContent);
+                    var res = await httpRes.Content.ReadAsStreamAsync();
+                    StreamReader reader = new StreamReader(res, Encoding.GetEncoding(51949));
+                    statusText.Text = reader.ReadToEnd();
+                } catch (Exception ex)
+                {
+                    statusText.Text = ex.ToString();
+                }
+            }
+            
+        }
+    }
+}
